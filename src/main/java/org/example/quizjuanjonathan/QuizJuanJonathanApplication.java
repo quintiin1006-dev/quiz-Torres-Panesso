9f6a2f58d372f892cfe19311034ad545b4c2258e
Juan Jonathan Panesso <juan.jonathan@gmail.com>
2026-04-13 10:48:30 -0500

    Initial commit - Spring Boot project

---
+++ src/main/java/org/example/quizjuanjonathan/QuizJuanJonathanApplication.java
@@ -0,0 +1,13 @@
+package org.example.quizjuanjonathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizJuanJonathanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizJuanJonathanApplication.class, args);
+    }
+
+}
